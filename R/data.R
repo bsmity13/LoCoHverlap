a8450ec64020a64143b15ced320406f69a59a3c4
bsmity13 <bsmity13@gmail.com>
2021-02-06 23:02:38 -0700

    Added examples for plotting sample data.

--- R/data.R
+++ R/data.R
@@ -16,4 +16,44 @@
 #'
 #' @source Data simulated by Brian J. Smith using \code{\link{bcrw}()}.
 #'
+#' @examples
+#' \dontrun{
+#'
+#' # Load ggplot2
+#' library(ggplot2)
+#'
+#' # Load data
+#' data(tracks)
+#'
+#' # View head
+#' head(tracks)
+#'
+#' # Plot map of tracks
+#' ggplot(tracks, aes(x = x, y = y, color = id)) +
+#'   geom_path() +
+#'   coord_sf() +
+#'   xlab(NULL) +
+#'   ylab(NULL) +
+#'   theme_bw()
+#'
+#' # Map winter only
+#' winter <- tracks[which(tracks$season == "Winter"), ]
+#' ggplot(winter, aes(x = x, y = y, color = id)) +
+#'   geom_path() +
+#'   coord_sf() +
+#'   xlab(NULL) +
+#'   ylab(NULL) +
+#'   theme_bw()
+#'
+#' # Map summer only
+#' summer <- tracks[which(tracks$season == "Summer"), ]
+#' ggplot(summer, aes(x = x, y = y, color = id)) +
+#'   geom_path() +
+#'   coord_sf() +
+#'   xlab(NULL) +
+#'   ylab(NULL) +
+#'   theme_bw()
+#'
+#' }
+#'
 "tracks"
