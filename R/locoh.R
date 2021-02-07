4b677029c0e03c9cfefcd02ccc6950311a9eee91
bsmity13 <bsmity13@gmail.com>
2021-02-06 17:08:26 -0700

    Initial package setup.

---
+++ R/locoh.R
@@ -0,0 +1,32 @@
+# Functions to fit LoCoH home ranges
+
+#' Fit one LoCoH home range
+#'
+#' Fits a single LoCoH home range to data
+#'
+#' @param dat `[data.frame]` The location data used for fitting the LoCoH.
+#' Should contain the following columns:
+#'   * `$x` -- The x-coordinate of the animal's location
+#'   * `$y` -- The y-coordinate of the animal's location
+#'   * `$t` -- The date and time (as a `POSIXct` object) of the location
+#' @param type `[character = "a"]` The type of neighbor rule used to create the
+#' local hulls. Should be one of `"a"` (default), `"k"`, or `"r"`.
+#' @param n `[numeric = NA]` The value to use for the neighbor rule, *i.e.*, the
+#' value of either a, k, or r used to fit the local hulls.
+#' @param `...` Additional arguments passed to \code{\link{amt::hr_locoh}()}.
+#'
+#' @details A wrapper for `amt` functions to fit LoCoH home ranges. First
+#' creates a `track_xyt` object, then calls \code{\link{amt::hr_locoh}()}.
+#'
+#' @seealso
+#' \code{\link{amt::hr_locoh}()}
+#' Getz et al. 2007 (*insert full citation here*)
+#'
+#' @examples
+#' \dontrun{
+#' # Example
+#' }
+#' @export
+fit_locoh <- function(dat, type, n, rand_buffer) {
+
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# LoCoHverlap
+
+<!-- badges: start -->
+
+[![Lifecycle:
+experimental](https://img.shields.io/badge/lifecycle-experimental-orange.svg)](https://www.tidyverse.org/lifecycle/#experimental)
+<!-- badges: end -->
+
+`LoCoHverlap` is an R package to compare animal range overlaps using
+Local Convex Hull (LoCoH) home range estimators.
+
+## Installation
+
+You can install the development version from
+[GitHub](https://github.com/) with:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("bsmity13/LoCoHverlap")
+```
